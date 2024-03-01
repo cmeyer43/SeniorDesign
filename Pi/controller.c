f31b56bf0cb386bafdf0bfd0f879473963b4c9cb
Corey <mail2coreym@gmail.com>
2024-03-01 12:16:29 -0500

    yep

--- Pi/controller.c
+++ Pi/controller.c
@@ -2,28 +2,50 @@
 #include "brakeZone2.h"
 #include "brakeZone3.h"
 #include "brakeZone4.h"
+#include "stateMachine.h"
 #include <pthread.h>
 #include <stdio.h>
 #include <stdint.h>
 
-void* brakeZone_1_2()
+void* brakeZone_1()
 {
+    brakeZone1Run();
 }
 
-void* brakeZone_3_4()
+void* brakeZone_2()
 {
+    brakeZone2Run();
+}
+
+void* brakeZone_3()
+{
+    brakeZone3Run();
+}
+
+void* brakeZone_4()
+{
+    brakeZone4Run();
 }
 
 int main()
 {
     int err = 0;
-    pthread_t brakeZone_1_2_thread;
-    pthread_t brakeZone_3_4_thread;
+    pthread_t brakeZone1Thread;
+    pthread_t brakeZone2Thread;
+    pthread_t brakeZone3Thread;
+    pthread_t brakeZone4Thread;
+    states_t state = STOP;
 
     err += brakeZone1Init();
     err += brakeZone2Init();
     err += brakeZone3Init();
     err += brakeZone4Init();
+
+    pthread_create(&brakeZone1Thread, NULL, &brakeZone_1, NULL);
+    pthread_create(&brakeZone2Thread, NULL, &brakeZone_2, NULL);
+    pthread_create(&brakeZone3Thread, NULL, &brakeZone_3, NULL);
+    pthread_create(&brakeZone4Thread, NULL, &brakeZone_4, NULL);
+
 
     if (err != 0)
     {
---
+++ Pi/stateMachine.h
@@ -0,0 +1,12 @@
+#ifndef STATEMACHINE_H
+#define STATEMACHINE_H
+
+typedef enum state
+{
+    AUTOMATIC = 0,
+    MANUAL,
+    MAITNENCE,
+    STOP
+} states_t;
+
+#endif
