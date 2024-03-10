bdfe310021a019f0ad04e47da5fbe41405db3790
Corey <mail2coreym@gmail.com>
2024-03-10 17:50:46 -0400

    State machine setup

---
+++ AtMega/arduinoSerialRead/stateMachine.h
@@ -0,0 +1,20 @@
+#ifndef STATEMACHINE_H
+#define STATEMACHINE_H
+
+enum
+{
+    AUTOMATIC = 0,
+    MANUAL,
+    MAITNENCE,
+    STOP
+};
+
+enum
+{
+    ENTERING = 0,
+    ENTERED,
+    EXITING,
+    EMPTY
+};
+
+#endif
